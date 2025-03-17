7dcb520ba9efc9bcfd446df333b95ecc969e4c9f
Arjunkumar32 <arjunkumarb070@gmail.com>
2025-03-17 21:32:17 +0530

    Login Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
-
+import LoginPage from "../pages/LoginPage";
 const AppRoutes = () => {
   return (
     <Routes>
      
       <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
