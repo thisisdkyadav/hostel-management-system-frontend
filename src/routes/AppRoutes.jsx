6593a3fd9282f649cce957693fbac6b0395f3cde
abhaylodhi014 <abhaylodhi128135@gmail.com>
2025-03-18 20:28:59 +0530

    guard section

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import SDashboard from "../pages/student/SDashboard";
 import LoginPage from "../pages/LoginPage";
 import WardenDashboard from "../pages/warden/DashboardWarden";
-
+import GuardDashboard from "../pages/guard/GDashboard"
+import AddVisitor from "../pages/guard/AddVisitor"
 const AppRoutes = () => {
   return (
     <Routes>
@@ -10,6 +11,8 @@ const AppRoutes = () => {
       <Route path="/" element={<SDashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/warden/dashboard" element={<WardenDashboard />} />
+      <Route path="/guard/dashboard" element={<GuardDashboard/>} />
+      <Route path="/AddVisitor/dashboard" element={<AddVisitor/>} />
     </Routes>
   );
 };
