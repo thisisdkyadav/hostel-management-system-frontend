ed09cb3f78c4cf4ac102bfa6da299238cf443b64
Deepanshu <guptadeepanshu9079@gmail.com>
2025-03-19 01:23:59 +0530

    Maintainance Portal

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import SDashboard from "../pages/student/SDashboard";
 import LoginPage from "../pages/LoginPage";
 import WardenDashboard from "../pages/warden/DashboardWarden";
-
+import MDashboard from "../pages/maintainance/MDashboard";
+import ScheduleM from "../components/Maintainance/ScheduleM";
+import ComplaintsPage from "../components/Maintainance/ComplaintsPage";
 const AppRoutes = () => {
   return (
     <Routes>
@@ -10,7 +12,10 @@ const AppRoutes = () => {
       <Route path="/" element={<SDashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/warden/dashboard" element={<WardenDashboard />} />
-    </Routes>
+      <Route path="/maintainance/dashboard" element={<MDashboard />} />
+      <Route path="/maintainance/complaints" element={<ComplaintsPage />} /> 
+      <Route path="/maintainance/schedule" element={<ScheduleM />} /> 
+</Routes>
   );
 };
 
