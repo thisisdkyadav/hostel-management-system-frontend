c060aa38452f32f80a58d4113eb70c8754d3edd8
abhaylodhi014 <abhaylodhi128135@gmail.com>
2025-03-18 20:36:27 +0530

    guard section

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,11 @@ import SDashboard from "../pages/student/SDashboard";
 import LoginPage from "../pages/LoginPage";
 import WardenDashboard from "../pages/warden/DashboardWarden";
 
+import GuardDashboard from "../pages/guard/GDashboard"
+import AddVisitor from "../pages/guard/AddVisitor"
+
+import Complaint from "../pages/warden/Complaint";
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -10,6 +15,12 @@ const AppRoutes = () => {
       <Route path="/" element={<SDashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/warden/dashboard" element={<WardenDashboard />} />
+
+      <Route path="/guard/dashboard" element={<GuardDashboard/>} />
+      <Route path="/AddVisitor/dashboard" element={<AddVisitor/>} />
+
+      <Route path="/warden/complaint" element={<Complaint/>} />
+
     </Routes>
   );
 };
