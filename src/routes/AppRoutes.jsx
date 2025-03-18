77ade0100f94e8ae233c9046d10baaa6ec0a619b
Arjunkumar32 <arjunkumarb070@gmail.com>
2025-03-18 20:03:04 +0530

    Complaint

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import SDashboard from "../pages/student/SDashboard";
 import LoginPage from "../pages/LoginPage";
 import WardenDashboard from "../pages/warden/DashboardWarden";
-
+import Complaint from "../pages/warden/Complaint";
 const AppRoutes = () => {
   return (
     <Routes>
@@ -10,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<SDashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/warden/dashboard" element={<WardenDashboard />} />
+      <Route path="/warden/complaint" element={<Complaint/>} />
     </Routes>
   );
 };
