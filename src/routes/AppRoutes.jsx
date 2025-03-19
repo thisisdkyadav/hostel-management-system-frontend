79d009b397de834b4b090156688df62e821b4112
Kaal-09 <sakshamgautam369@gmail.com>
2025-03-19 09:22:08 +0530

    temp

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,30 +1,52 @@
-import { Routes, Route } from "react-router-dom";
-import SDashboard from "../pages/student/SDashboard";
-import LoginPage from "../pages/LoginPage";
-import WardenDashboard from "../pages/warden/DashboardWarden";
-import Complaints from "../pages/student/Complaints";
+import { Routes, Route } from "react-router-dom"
+import SDashboard from "../pages/student/SDashboard"
+import LoginPage from "../pages/LoginPage"
+import WardenDashboard from "../pages/warden/DashboardWarden"
+import Complaints from "../pages/student/Complaints"
 
 import GuardDashboard from "../pages/guard/GDashboard"
 import AddVisitor from "../pages/guard/AddVisitor"
-
-import Complaint from "../pages/warden/Complaint";
+import Complaint from "../pages/warden/Complaint"
+// import related to maintainance
+import MDashboard from "../pages/maintainance/MDashboard"
+import ScheduleM from "../components/Maintainance/ScheduleM"
+import ComplaintsPage from "../components/Maintainance/ComplaintsPage"
+import AdminLayout from "../layouts/AdminLayout"
+import AdminDashboard from "../pages/admin/Dashboard"
+import AdminHostels from "../pages/admin/Hostels"
+import AdminWarden from "../pages/admin/Wardens"
+import AdminStudents from "../pages/admin/Students"
+import AdminComplaints from "../pages/admin/Complaints"
+import Profile from "../pages/student/Profile"
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Routes for student related pages  */}
-      <Route path="/api/v0/student/dashboard" element={<SDashboard/>} />
+      <Route path="/api/v0/student/dashboard" element={<SDashboard />} />
       <Route path="/api/v0/student/complaints" element={<Complaints />} />
-          
+      <Route path="/api/v0/student/profile" element={<Profile />} />
+
       <Route path="/" element={<SDashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/warden/dashboard" element={<WardenDashboard />} />
 
-      <Route path="/guard/dashboard" element={<GuardDashboard/>} />
-      <Route path="/AddVisitor/dashboard" element={<AddVisitor/>} />
+      <Route path="/guard/dashboard" element={<GuardDashboard />} />
+      <Route path="/AddVisitor/dashboard" element={<AddVisitor />} />
 
-      <Route path="/warden/complaint" element={<Complaint/>} />
-        
+      <Route path="/warden/complaint" element={<Complaint />} />
+      {/* Routes for maintenance related pages*/}
+      <Route path="/maintainance/dashboard" element={<MDashboard />} />
+      <Route path="/maintainance/complaints" element={<ComplaintsPage />} />
+      <Route path="/maintainance/schedule" element={<ScheduleM />} />
+
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="hostels" element={<AdminHostels />} />
+        <Route path="wardens" element={<AdminWarden />} />
+        <Route path="students" element={<AdminStudents />} />
+        <Route path="complaints" element={<AdminComplaints />} />
+      </Route>
     </Routes>
   )
 }
