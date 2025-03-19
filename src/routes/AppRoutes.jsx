a0ac3efecc86879e34a38a0c8b956a1b2693ec97
Deepanshu <guptadeepanshu9079@gmail.com>
2025-03-19 19:40:16 +0530

    mp-small fix

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -12,7 +12,6 @@ import DataPage from "../pages/warden/DataPage";
 import MDashboard from "../pages/maintainance/MDashboard"
 import ScheduleM from "../pages/maintainance/ScheduleM"
 import ComplaintsPage from "../pages/maintainance/ComplaintsPage"
-import Notification from "../components/Maintainance/NotificationM"
 // import related to admin
 import AdminLayout from "../layouts/AdminLayout"
 import AdminDashboard from "../pages/admin/Dashboard"
