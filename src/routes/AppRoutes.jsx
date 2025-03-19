25d2cf9966a975dbbc1d48724d0e297677fd4ace
Devesh Yadav <thisisdkyadav@gmail.com>
2025-03-20 00:13:52 +0530

    Update AppRoutes to include new student profile and lost-and-found routes, and organize existing routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,7 +10,7 @@ import LostAndFound from "../pages/student/LostAndFound.jsx"
 import GuardDashboard from "../pages/guard/GDashboard"
 import AddVisitor from "../pages/guard/AddVisitor"
 import Complaint from "../pages/warden/Complaint"
-import DataPage from "../pages/warden/DataPage";
+import DataPage from "../pages/warden/DataPage"
 // import related to maintainance
 import MDashboard from "../pages/maintainance/MDashboard"
 import ScheduleM from "../pages/maintainance/ScheduleM"
@@ -24,18 +24,39 @@ import AdminWarden from "../pages/admin/Wardens"
 import AdminStudents from "../pages/admin/Students"
 import AdminComplaints from "../pages/admin/Complaints"
 
-
 const AppRoutes = () => {
   return (
     <Routes>
-       {/* Routes for student related pages  */}
-        <Route path="/api/v0/student/dashboard" element={<SDashboard/>} />
-        <Route path="/api/v0/student/complaints" element={<Complaints />} />
-        
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/warden/dashboard" element={<WardenDashboard />} />
-    </Routes>
-  );
-};
+      <Route path="/api/v0/student/dashboard" element={<SDashboard />} />
+      <Route path="/api/v0/student/complaints" element={<Complaints />} />
+      <Route path="/api/v0/student/profile" element={<Profile />} />
+      <Route path="/api/v0/student/lost-and-found" element={<LostAndFound />} />
 
-export default AppRoutes;
+      <Route path="/" element={<SDashboard />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/warden/dashboard" element={<WardenDashboard />} />
+      <Route path="/warden/data" element={<DataPage />} />
+
+      <Route path="/guard/dashboard" element={<GuardDashboard />} />
+      <Route path="/AddVisitor/dashboard" element={<AddVisitor />} />
+      <Route path="/warden/complaint" element={<Complaint />} />
+
+      {/* Routes for maintenance related pages*/}
+      <Route path="/maintainance/dashboard" element={<MDashboard />} />
+      <Route path="/maintainance/complaints" element={<ComplaintsPage />} />
+      <Route path="/maintainance/schedule" element={<ScheduleM />} />
+      <Route path="/maintainance/alert" element={<AlertPage />} />
+
+      {/* Routes for admin related pages */}
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="hostels" element={<AdminHostels />} />
+        <Route path="wardens" element={<AdminWarden />} />
+        <Route path="students" element={<AdminStudents />} />
+        <Route path="complaints" element={<AdminComplaints />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRoutes
