cf1eb775d11e4528e03b3a6eee35d05e4df0e42b
Devesh Yadav <thisisdkyadav@gmail.com>
2025-03-27 21:43:14 +1100

    feat: add PWA support with vite-plugin-pwa and update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
+import { VitePWA } from "vite-plugin-pwa"
 
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My App",
+        short_name: "App",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/IITILogo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/IITILogo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      // devOptions: {
+      //   enabled: true, // Enables PWA in development mode
+      // },
+    }),
+  ],
   base: "/", // Ensures relative paths for assets
 })
