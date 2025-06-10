d7cff4d37b3d3091bf2a5138f2f943e517bb79c9
Devesh Yadav <thisisdkyadav@gmail.com>
2025-06-10 10:47:39 +0530

    feat: add environment configuration and update API base URL
    - Introduced a new .env file for environment variables, including SSO server and API URLs.
    - Updated appConstants.js to use the VITE_API_URL from the environment variables, ensuring flexibility for different environments.

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -5,6 +5,4 @@ export const fetchOptions = {
   },
 }
 
-// export const baseUrl = "https://hostel-management-system-backend-and4hrevaag3f5gs.centralindia-01.azurewebsites.net/api"
-export const baseUrl = "http://localhost:5000/api"
-// export const baseUrl = "https://9m64jhxk-5000.inc1.devtunnels.ms/api"
+export const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
