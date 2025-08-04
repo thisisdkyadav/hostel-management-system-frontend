5b07028ef9231d893a292a1ae3a2ca1354199a4f
Devesh Yadav <thisisdkyadav@gmail.com>
2025-08-04 18:32:52 +0530

    fix: update service worker revision for index.html
    - Changed the revision identifier for index.html in the service worker to ensure the latest version is cached and served to users.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.koia2n1i558"
+    "revision": "0.n2s6se226u"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
