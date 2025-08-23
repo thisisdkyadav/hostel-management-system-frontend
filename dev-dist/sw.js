664586c9c5da362ed01423220a671b91e3f057ea
Devesh Yadav <thisisdkyadav@gmail.com>
2025-08-23 11:49:39 +0530

    fix: update maximum records limit in BulkStudentInsuranceModal to 900
    - Adjusted the error message in the BulkStudentInsuranceModal component to reflect the new maximum records limit of 900 for free accounts, ensuring users are accurately informed of the restrictions.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.n2s6se226u"
+    "revision": "0.bn6us8ehq5s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
