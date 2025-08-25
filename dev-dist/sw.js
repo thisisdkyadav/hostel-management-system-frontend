b98e1717e85f8664298335335b83e6313024ee2b
Devesh Yadav <thisisdkyadav@gmail.com>
2025-08-25 11:31:25 +0530

    fix: update Dashboard component for code consistency and performance
    - Refactored the Dashboard component to improve code consistency by standardizing quotation marks and simplifying JSX structure.
    - Updated the service worker revision for index.html to ensure the latest version is cached.
    - Enhanced shimmer loader components for better visual feedback during loading states.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.r9miuoetpfk"
+    "revision": "0.lodb8f2nh54"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
