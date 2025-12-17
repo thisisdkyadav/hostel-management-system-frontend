f226ff34f92fef47648b9c53dc85c7550900a8c7
Devesh Yadav <thisisdkyadav@gmail.com>
2025-12-17 15:09:18 +0530

    feat: add FeedbackHeader component and remove deprecated StudentFeedback page

---
+++ src/components/headers/FeedbackHeader.jsx
@@ -0,0 +1,20 @@
+import PageHeader from "../common/PageHeader"
+import Button from "../common/Button"
+import { HiPlus } from "react-icons/hi"
+
+const FeedbackHeader = ({ userRole, onAddFeedback }) => {
+  const isStudent = userRole === "Student"
+  const title = isStudent ? "My Feedbacks" : "Student Feedbacks"
+
+  return (
+    <PageHeader title={title}>
+      {isStudent && (
+        <Button variant="primary" size="medium" onClick={onAddFeedback} icon={<HiPlus size={14} />}>
+          Add Feedback
+        </Button>
+      )}
+    </PageHeader>
+  )
+}
+
+export default FeedbackHeader
