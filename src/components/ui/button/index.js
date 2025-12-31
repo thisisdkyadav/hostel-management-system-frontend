115b23d0e2293248c811895b0c8667b2d43d78c5
Devesh Yadav <thisisdkyadav@gmail.com>
2025-12-31 19:37:27 +0530

    feat: Add ToggleButtonGroup and UnderlineTabs components with documentation and export structure

--- src/components/ui/button/index.js
+++ src/components/ui/button/index.js
@@ -4,3 +4,4 @@
 export { default as Button } from './Button'
 export { default as IconButton } from './IconButton'
 export { default as ButtonGroup } from './ButtonGroup'
+export { default as ToggleButtonGroup } from './ToggleButtonGroup'
--- src/components/ui/navigation/index.js
+++ src/components/ui/navigation/index.js
@@ -2,5 +2,6 @@
  * Navigation Components Export
  */
 export { default as Tabs } from './Tabs'
+export { default as UnderlineTabs } from './UnderlineTabs'
 export { default as Pagination } from './Pagination'
 export { default as Breadcrumb } from './Breadcrumb'
