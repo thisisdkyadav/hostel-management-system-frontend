83b0b4e2e4480bce65d8011f140837e473105314
Devesh Yadav <thisisdkyadav@gmail.com>
2026-01-01 00:39:40 +0530

    Remove Warden Dashboard, Feedbacks, Student Inventory, and Undertakings components

--- src/pages/admin/HostelSupervisors.jsx
+++
@@ -1,7 +0,0 @@
-import StaffManagement from "../../components/admin/staff/StaffManagement"
-
-const AdminHostelSupervisors = () => {
-  return <StaffManagement staffType="hostelSupervisor" />
-}
-
-export default AdminHostelSupervisors 
